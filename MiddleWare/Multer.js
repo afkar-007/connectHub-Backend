98fdccbfc693480eeb5e5bdc73cb6bacc1ba0a6f
afkar-007 <afkar3547@gmail.com>
2026-09-23 14:21:49 +0530

    Add Cloudinary image upload

--- MiddleWare/Multer.js
+++ MiddleWare/Multer.js
@@ -1,5 +1,5 @@
 const multer = require("multer")
-const {cloudinaryStorage, CloudinaryStorage}=require("multer-storage-cloudinary")
+const { CloudinaryStorage}=require("multer-storage-cloudinary")
 const cloudinary = require("../config/cloudinary")
 
 const storage = new CloudinaryStorage({
